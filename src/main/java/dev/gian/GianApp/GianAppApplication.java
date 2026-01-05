6e02010b862819b00c3b0cfad797301f8c1430a7
WesleyBule <bulewesley7@gmail.com>
2026-01-05 22:57:10 +0200

    initial commit

---
+++ src/main/java/dev/gian/GianApp/GianAppApplication.java
@@ -0,0 +1,13 @@
+package dev.gian.GianApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GianAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GianAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gian/GianApp/GianAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gian.GianApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GianAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
