71df00d03dea5899daeb5d4b23eb11b3cafeba90
WesleyBule <bulewesley7@gmail.com>
2026-01-05 23:37:54 +0200

    Home Panel added

---
+++ src/main/java/dev/gian/GianApp/controllers/Controller.java
@@ -0,0 +1,17 @@
+package dev.gian.GianApp.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index(){
+        return "Home Panel";
+    }
+
+
+
+}
